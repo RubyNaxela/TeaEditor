7c3731d5dd51f97df80aaea38b8fc97e6355e082
RubyNaxela <rubynaxela@gmail.com>
2020-11-11 23:31:38 +0100

    [beta-1.4.2_719] Fixed grams data validation

--- src/main/java/com/rubynaxela/teaeditor/util/DataFormat.java
+++ src/main/java/com/rubynaxela/teaeditor/util/DataFormat.java
@@ -89,7 +89,8 @@ public class DataFormat {
      * @return {@code true} if the {@code String} is valid grams data and {@code false} otherwise
      */
     public static boolean isValidGramsData(@Nonnull String data) {
-        return data.matches("^(\\d+:\\d+)?(\\n\\d+:\\d+)*$");
+        return data.matches("^(((?!^\\.$)(?!^([\\d]*\\.[\\d]*){2,}$)[\\d.])+:\\d+)?" +
+                "(\\n((?!^\\.$)(?!^([\\d]*\\.[\\d]*){2,}$)[\\d.])+:\\d+)*$");
     }
 
     /**
